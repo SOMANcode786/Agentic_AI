603dc5002a13aa20abe43ba9dfeb7d84aa39753d
Muhammad Soman <somanamir43@gmail.com>
2025-07-28 13:00:20 +0500

    up

--- 06_Tool_Calling/01_basic_tool/main.py
+++ 06_Tool_Calling/01_basic_tool/main.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 from agents import (
     Agent,                           # 🤖 Core agent class
     Runner,                          # 🏃 Runs the agent
@@ -28,3 +29,29 @@ result=Runner.run_sync(
 
 print("The result is : -- > : ",result.final_output)
 
+=======
+from agents import Agent, Runner, AsyncOpenAI, OpenAIChatCompletionsModel
+from agents.run import RunConfig
+from dotenv import load_dotenv
+import os
+import asyncio
+
+load_dotenv()
+
+gemini_api_key=os.getenv("GEMINI_API_KEY")
+client=AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+model = OpenAIChatCompletionsModel(
+    model="gemini-1.5-flash",  # Valid Gemini model
+    openai_client=external_client
+)
+config = RunConfig(
+    model=model,
+    tracing_disabled=True
+    )
+
+
+>>>>>>> Stashed changes
