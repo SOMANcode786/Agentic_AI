32015b4018b7c85fcb0f7d50b18d2c51edc2902d
Muhammad Soman <somanamir43@gmail.com>
2025-08-15 01:48:48 +0500

    up

--- Practice/context_practice/main.py
+++ Practice/context_practice/main.py
@@ -11,4 +11,30 @@ class context:
 
 
 @function_tool
-async def 
+async def fetch_user_age(wrappert: RunContextWrapper[context]) -> int:
+    return f"user {wrappert.context.name} is 30 years old."
+
+async def main():
+    user_context = context(name="Alice", description="A user interested in AI agents.")
+
+    agent=Agent(
+        name="user context agent",
+        instructions="You are an agent that uses user context to provide personalized responses.",
+        tools=[fetch_user_age],
+        context=user_context
+    )
+
+    result=await Runner.run_async(
+        agent,
+        "What is my age?",
+        run_context=RunContextWrapper(context=user_context)
+    ) 
+
+
+    print(f"User's age: {result.final_output}")
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
+else:
+    print("This script is intended to be run directly.")
