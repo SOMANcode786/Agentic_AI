4921c1664d8de235971305ec6eb8451ca06d38c8
Muhammad Soman <165492635+Muhammad-soman@users.noreply.github.com>
2025-09-29 16:35:00 +0500

    update

--- Paper_practice/openai_api/main.py
+++ Paper_practice/openai_api/main.py
@@ -1,8 +1,26 @@
-from agents import Agent, Runner, handoff
+from agents import Agent, RunConfig, Runner, handoff, AsyncOpenAI, OpenAIChatCompletionsModel
 import asyncio
 from dotenv import load_dotenv
-
+import os
 load_dotenv()
+
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+external_client = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+)
+
+model = OpenAIChatCompletionsModel(
+    model="gemini-2.0-flash",
+    openai_client=external_client
+)
+
+config = RunConfig(
+    model=model,
+    model_provider=external_client,
+    tracing_disabled=True
+)
+print(load_dotenv())
 
 billing_agent = Agent(
     name="Billing agent",
@@ -28,7 +46,7 @@ triage_agent = Agent(
 )
 
 async def main():
-    result = await Runner.run(triage_agent, "I need to check refund status.")
+    result = await Runner.run(triage_agent, "I need to check refund status.",run_config=config)
     print("Final Output:", result.final_output)
 
 if __name__ == "__main__":
